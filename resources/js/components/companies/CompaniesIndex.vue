ec8c3715b0b03f73e77bd765255b2e6305f0a8ee
Dio <dionatsir@gmail.com>
2022-03-11 16:19:06 +0700

    first commit

---
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -0,0 +1,16 @@
+<template>Hello World</template>
+
+<script>
+import useCompanies from "../../composables/companies";
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    const { companies, getCompanies } = useCompanies();
+
+    onMounted(getCompanies)
+
+    return { companies };
+  },
+};
+</script>
